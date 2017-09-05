cff02f83621ef705df4714fbdd85d4e201236f09
YZ <741529838@qq.com>
2017-09-05 15:46:10 +0800

    2

---
+++ lesson1.js
@@ -0,0 +1,24 @@
+//boolean
+var isTrue = true;
+//number
+var data1 = 8;
+var data2 = 0xf00d;
+var data3 = 10;
+var data4 = 484;
+//string
+var str1 = "hello boy";
+var str2 = 'hello girl';
+var str3 = "hello world";
+var str4 = "Hello world " + str3;
+//null
+var n = null;
+//undefined
+var a = undefined;
+//void空值
+function say() {
+    alert('hello world');
+}
+var num = 10;
+num = undefined;
+num = null;
+//num = 'hello';
---
+++ lesson1.ts
@@ -0,0 +1,39 @@
+//boolean
+let isTrue: boolean = true;
+
+//number
+let data1:number = 8;
+
+let data2:number = 0xf00d;
+
+let data3:number = 0b1010;
+
+let data4:number = 0o744;
+
+//string
+let str1:string = "hello boy";
+
+let str2:string = 'hello girl';
+
+let str3:string = `hello world`;
+
+let str4:string = `Hello world ${str3}`;
+
+//null
+let n:null = null;
+
+//undefined
+let a:undefined=undefined;
+
+//void空值
+function say():void{
+    alert('hello world');
+}
+
+let num:number = 10;
+
+num = undefined;
+
+num = null;
+
+//num = 'hello';
